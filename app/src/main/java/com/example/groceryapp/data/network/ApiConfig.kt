0b5de8cf29d780d700079c144fed88e70551c229
Abdulla <a24998113@gmail.com>
2026-09-05 16:46:21 +0500

    url issue solved

--- app/src/main/java/com/example/groceryapp/data/network/ApiConfig.kt
+++ app/src/main/java/com/example/groceryapp/data/network/ApiConfig.kt
@@ -1,6 +1,6 @@
 package com.example.groceryapp.data.network
 
 object ApiConfig {
-    // 192.168.0.102 is the IPv4 address of the host machine for physical device access
-    const val BASE_URL = "http://192.168.0.102:8080"
+    // 192.168.0.104 is the IPv4 address of the host machine for physical device access
+    const val BASE_URL = "http://192.168.0.104:8080"
 }
